396a43f64c159f08b83be96fa42ef7ef1707854f
Sandeep Suneja <sunny@Sandeeps-MacBook-Pro.local>
2024-04-01 03:50:58 +0530

    Get Bus API integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'bus-ticket-booking-FE';
+export class AppComponent implements OnInit {
+
+  title = 'Bus Ticket Booking System';
+  buses: any = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5047/api/Bus').subscribe((data) => {
+      this.buses = data;
+    });
+  }
 }
